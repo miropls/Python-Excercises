0455a8667ada5f9d9fdb35f9f7f923ae583a0cb5
Miro Paintola <miro.paintola@gmail.com>
2019-12-21 01:42:18 +0200

    Added comments to describe the programs.

--- list_less_than_five.py
+++ list_less_than_five.py
@@ -1,3 +1,5 @@
+# Program where a function creates a list of 20 random numbers between 1 and 25, and a second function prints out a list with all the numbers under the value of 5.
+
 import random
 
 number_list = []
